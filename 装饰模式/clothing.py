8b2e117fb406a7c54ad1eef0b124751229eaf823
karlblom3854 <karlblom3854@gmail.com>
2024-01-14 21:29:18 +0800

    装饰模式-练习

--- 装饰模式/clothing.py
+++ 装饰模式/clothing.py
@@ -3,11 +3,11 @@ from abc import ABCMeta, abstractmethod
 
 class Person(metaclass=ABCMeta):
     def __init__(self, name):
-        self.__name = name
+        self._name = name
 
     @abstractmethod
     def wear(self):
-        print("着装")
+        print("着装：")
 
 
 class Engineer(Person):
@@ -15,17 +15,24 @@ class Engineer(Person):
         super().__init__(name)
         self.__skill = skill
 
+    def get_skill(self):
+        return self.__skill
+
     def wear(self):
-        print(f"我是{self.__skill}工程师{self.__name}", end="，")
+        print(f"我是{self.get_skill()}工程师{self._name}", end="，")
         super().wear()
+
 
 class Teacher(Person):
     def __init__(self, name, title):
         super().__init__(name)
         self.__title = title
 
+    def get_title(self):
+        return self.__title
+
     def wear(self):
-        print(f"我是{self.__title}{self.__name}", end="，")
+        print(f"我是{self.get_title()}{self._name}", end="，")
         super().wear()
 
 
@@ -40,4 +47,67 @@ class ClothingDecorator(Person):
     @abstractmethod
     def decorate(self):
         pass
+
+
+class CausalPantDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t休闲裤")
+
+
+class BeltDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t腰带")
+
+
+class LeatherShoeDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t皮鞋")
+
+
+class KnittedSweeterDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t针织毛衣")
+
+
+class WhiteShirtDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t白色衬衫")
+
+
+class GlassDecorator(ClothingDecorator):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def decorate(self):
+        print("\t眼镜")
+
+
+if __name__ == '__main__':
+    tony = Engineer("Tony", "前端")
+    pant = CausalPantDecorator(tony)
+    belt = BeltDecorator(pant)
+    shoes = LeatherShoeDecorator(belt)
+    shirt = WhiteShirtDecorator(shoes)
+    sweater = KnittedSweeterDecorator(shirt)
+    glasses = GlassDecorator(sweater)
+    glasses.wear()
+
+    carl = GlassDecorator(CausalPantDecorator(WhiteShirtDecorator(LeatherShoeDecorator(
+        Teacher("Carl", "教授")))))
+    carl.wear()
 
